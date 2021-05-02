f9756db5b494f821367abba456a2e8d803f35218
Iruiasaro <brunettirosario@gmail.com>
2021-05-02 17:01:21 +0200

    scheletro ed immagine default

---
+++ script.js
@@ -0,0 +1,14 @@
+const app = new Vue({
+
+    el: '#app',
+    data: {
+        ImgZero: 0,
+
+        imgHarley: [
+            { url: 'img/harley0.jpeg' },
+            { url: 'img/harley1.jpeg' },
+            { url: 'img/harley2.jpeg' },
+            { url: 'img/harley3.jpeg' },
+        ],
+    },
+});
